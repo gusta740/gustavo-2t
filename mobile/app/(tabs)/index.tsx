b735e69224fbd41db2e7980b04d23d4fc572b3bf
daniellyk <danielly.grando@escola.pr.gov.br>
2025-04-04 12:06:34 -0300

    Primeiro commit

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image, StyleSheet, Platform } from 'react-native';
-
+import {Alert, Button,  View} from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -8,7 +8,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#f8f8ff', light: '#f8f8ff' }}
+      headerBackgroundColor={{ light: '#f8f8ff', dark: '#f8f8ff' }}
       headerImage={
         <Image
           source={require('@/assets/images/images.png')}
@@ -22,16 +22,22 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Saldo:R$ 323,91</ThemedText>
         <ThemedText>
-          editavel <ThemedText type="defaultSemiBold">editavel</ThemedText> editavel
-          editalvel{' '}
+          <ThemedText type="defaultSemiBold"></ThemedText>
+          {''}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
-              web: 'editavel'
+              web: ''
             })}
           </ThemedText>{' '}
-          editavel
+          <Button color={"#04971a"}
+  onPress={() => {
+    console.log('You tapped the button!');
+  }}
+  title="PIX"
+  
+/>
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -44,10 +50,10 @@ export default function HomeScreen() {
         <ThemedText type="subtitle">cartão de credito internacional</ThemedText>
         <ThemedText>
           R$3000{' '}
-          <ThemedText type="defaultSemiBold"></ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          <ThemedText type="defaultSemiBold"></ThemedText> {' '}
+          <ThemedText type="defaultSemiBold"></ThemedText> {' '}
+          <ThemedText type="defaultSemiBold"></ThemedText> {' '}
+          <ThemedText type="defaultSemiBold"></ThemedText>.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -65,8 +71,8 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: "60%",
+    width: "100%",
     bottom: 0,
     left: 0,
     position: 'absolute',
